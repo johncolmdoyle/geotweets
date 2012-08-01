247d5e94cac2132c84d5713eca00f0272a66fe53
John Doyle <john.c.doyle@gmail.com>
2012-07-31 21:50:04 -0400

    Default configuration file

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+/** MYSQL Details */
+$MYSQL_USERNAME = "";
+$MYSQL_HOST = "";
+$MYSQL_PASSWORD = "";
+$MYSQL_DATABASE = "";
+
+/** Twitter API details */
+$TWITTER_CONSUMER_KEY = "";
+$TWITTER_CONSUMER_SECRET = "";
+$TWITTER_USER_TOKEN = "";
+$TWITTER_USER_SECRET = "";
+
+/** Google API details */
+$GOOGLE_MAPS_HOSTR = "maps.google.com";
+$GOOGLE_KEY = "";
+
+/** Root Directory */
+$ROOT_DIR = "";
+
+?>
