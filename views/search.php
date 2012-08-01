51759150e05c44b3ce12c09c4112819d56bb5702
John Doyle <john.c.doyle@gmail.com>
2012-07-31 23:45:57 -0400

    Updated Search View to connect to get Folowers controller.

--- views/search.php
+++ views/search.php
@@ -1,5 +1,9 @@
 <center><img height="300px" src="images/internationalTweets.png"></center>
 <div id="geotweets">
     <h1>Geotweets</h1>
-    <p>Searching....</p>
+<?php
+
+include($ROOT_DIT . '/controllers/getFollowers.php');
+
+?>
 </div>
